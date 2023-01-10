fc3225c038978df20b6c75862943cd62ffd579a1
Daniel Kelly <me@danielkelly.io>
2023-01-10 10:30:58 -0600

    vue 2 project that could use migrating to Vue 3

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,93 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <AppHeader level="1">Example Migration App</AppHeader>
+
+    <!-- native and v-model -->
+    <div>
+      <h2>v-on.native modifier removed and v-model changes</h2>
+      <AppInput v-model="msg" @click.native="handleAppInputClicked" />
+      <hr />
+    </div>
+
+    <!-- key and transition class -->
+    <div>
+      <h2>Key Usage and Transition Class Change</h2>
+      <label> <input type="checkbox" v-model="showMsg" /> Show Message </label>
+
+      <transition>
+        <div v-if="showMsg" key="yes">{{ msg | caps }}</div>
+        <div v-else key="no">Message Hidden</div>
+      </transition>
+      <hr />
+    </div>
+
+    <!-- v-bind merge -->
+    <div>
+      <h2>
+        v-bind Merge Behavior <span id="red" v-bind="{ id: 'blue' }"></span>
+      </h2>
+      <hr />
+    </div>
+
+    <!-- directives -->
+    <div v-click-outside="() => (showMenu = false)">
+      <h2>Custom Directives changes</h2>
+      <button @click="showMenu = !showMenu">
+        {{ showMenu ? "Hide" : "Show" }} Menu
+      </button>
+      <ul v-show="showMenu">
+        <li>Menu Item 1</li>
+        <li>Menu Item 2</li>
+        <li>Menu Item 3</li>
+      </ul>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import AppHeader from "./components/AppHeader.vue";
+import AppInput from "./components/AppInput.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    AppHeader,
+    AppInput,
+  },
+  data() {
+    return {
+      msg: "Hello World",
+      showMsg: true,
+      showMenu: false,
+    };
+  },
+  directives: {
+    clickOutside: {
+      bind(el, binding) {
+        el.__ClickOutsideHandler__ = (event) => {
+          if (!(el === event.target || el.contains(event.target))) {
+            binding.value(event);
+          }
+        };
+        document.body.addEventListener("click", el.__ClickOutsideHandler__);
+      },
+      unbind(el) {
+        document.body.removeEventListener("click", el.__ClickOutsideHandler__);
+      },
+    },
+  },
+  filters: {
+    caps(value) {
+      return value.toUpperCase();
+    },
+  },
+
+  methods: {
+    handleAppInputClicked() {
+      console.log("app input clicked");
+    },
+  },
+};
 </script>
 
 <style>
@@ -21,8 +95,28 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  padding: 40px;
+}
+
+.v-enter-active,
+.v-leave-active {
+  transition: opacity 0.5s ease;
+}
+
+.v-enter,
+.v-leave-to {
+  opacity: 0;
+}
+#red,
+#blue {
+  height: 10px;
+  width: 10px;
+  background: blue;
+  display: inline-block;
+}
+
+#red {
+  background: red;
 }
 </style>
---
+++ src/components/AppInput.vue
@@ -0,0 +1,14 @@
+<template>
+  <input
+    type="text"
+    :value="value"
+    @input="$emit('input', $event.target.value)"
+  />
+</template>
+<script>
+export default {
+  props: {
+    value: String,
+  },
+};
+</script>
